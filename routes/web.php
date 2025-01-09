59ae07769a54f58969aba138128ab07535574ce0
webuxmotion <pereverziev.andrii@gmail.com>
2025-01-09 16:22:43 +0200

    Api Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('hello', function() {
+    return response("<h1>Hello world!</h1>", 200)
+        ->header('Content-Type', 'text/plain')
+        ->header('foo', 'bar');
+});
+
+Route::get('/posts/{id}', function($id) {
+    ddd($id);
+    return response('Post ' . $id);
+})->where('id', '[0-9]+');
+
+Route::get('/search', function(Request $request) {
+    dd($request->name . " from " . $request->city);
 });
