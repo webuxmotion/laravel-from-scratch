598c06013edc8ef601675473e7fad0c204894097
webuxmotion <pereverziev.andrii@gmail.com>
2025-02-14 03:01:14 +0200

    Show styled menu

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,13 +1,20 @@
-<ul class="menu">
-    @foreach($tree as $node)
-        <li>
-            <a href="#">{{ $node['title'] }}</a>
-
-            @if($node['children']->isNotEmpty())
-                <ul class="submenu">
-                    @include('components.menu', ['tree' => $node['children']])
-                </ul>
+<div class="top-nav">
+    <ul class="memenu skyblue">
+        <li class="active"><a href="/">Home</a></li>
+        @foreach ($tree as $node)
+            @if ($node['children']->isNotEmpty())
+                <li class="grid"><a href="#">{{ $node->title }}</a>
+                    <div class="mepanel">
+                        <div class="row">
+                            <div class="col1 me-one">
+                                @include('components.submenu', ['tree' => $node['children']])
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            @else
+                <li class="grid"><a href="/category/{{ $node->alias }}">{{ $node->title }}</a></li>
             @endif
-        </li>
-    @endforeach
-</ul>
+        @endforeach
+    </ul>
+</div>
