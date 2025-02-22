3c00496f35849bfddfd0061c04494c5dcc0a4796
webuxmotion <pereverziev.andrii@gmail.com>
2025-02-22 21:52:01 +0200

    Save Order Product

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -21,7 +21,19 @@ class Order extends Model
 
     public static function saveOrderProduct($order_id)
     {
-        
+        $items = Cart::getCartItems();
+
+        foreach ($items as $key => $item) {
+            $productId = explode('-', $key)[0];
+    
+            OrderProduct::create([
+                'order_id'  => $order_id,
+                'product_id' => $productId,
+                'qty'       => $item['quantity'],
+                'title'     => $item['title'],
+                'price'     => $item['price'],
+            ]);
+        }
     }
 
     public static function mailOrder($order_id, $user_email)
